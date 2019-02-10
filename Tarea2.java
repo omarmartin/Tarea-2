a622dffd7db2d1215f8fe5b322b56aa38a28d4f0
Benjamín Anguiano Morales <47484163+BenjiAM78@users.noreply.github.com>
2019-02-09 18:44:30 -0600

    Update Tarea2.java

--- Tarea2.java
+++ Tarea2.java
@@ -2,6 +2,7 @@
 
 public class Tarea2 {
 	public static void main(String[] args) {
-		System.out.println("Hola mundo, por Omar Martín");
+		System.out.println("Hola mundo, por Omar MartÃ­n");
+		System.out.println("Hola mundo, por Benjamin anguiano");
 	}
 }
