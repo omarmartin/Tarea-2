f5e7ce93b3585c49dcc55773f9765a6477bfb652
AdrianCalzada <47489962+AdrianCalzada@users.noreply.github.com>
2019-02-09 21:15:47 -0600

    Update Tarea2.java

--- Tarea2.java
+++ Tarea2.java
@@ -4,5 +4,6 @@ public class Tarea2 {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo, por Omar Martín");
 		System.out.println("Hola mundo, por Benjamin anguiano");
+		System.out.println("Hola mundo, por Adrian Calzada");
 	}
 }
